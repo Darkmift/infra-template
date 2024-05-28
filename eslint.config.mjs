684d330106f77069623d8c3022a2b10e00eaae1e
Darkmift <darkmift@gmail.com>
2024-05-28 13:56:18 +0300

    cross platform configs

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['*/client/playwright.config.ts'],
+    rules: {
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+    },
+    // "extends": ["plugin:prettier/recommended"]
+  },
+  eslintPluginPrettierRecommended,
+]
